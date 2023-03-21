be35ffdef43b863fc935546caebf47a5aa81a482
CarmenTrual <carmen.trujillo.alvarez@hotmail.com>
2023-03-21 14:05:17 +0100

    Tarea Java

--- Capítulo 9/Tarea_C9.2/Ejercicio1/ArrayCats.java
+++ Capítulo 9/Tarea_C9.2/Ejercicio1/ArrayCats.java
@@ -1,19 +1,25 @@
+/**
+ * @author Carmen Trujillo Álvarez
+ * @file: ArrayCats.java
+ * @info: programa que crea un array de cuatro gatos e introduce los datos
+ *        de cada uno de ellos mediante un bucle y los muestra.
+ */
+
 public class ArrayCats {
   public static void main(String[] args) {
     Gato[] gato = new Gato[4];
 
-    // Introducir los datos de cada gato
+    // bucle para introducir los datos de cada gato
     for (int i = 0; i < gato.length; i++) {
-      gato[i] = new Gato("Gato " + (i+1), i+1, i%2==0?"Macho":"Hembra");
+      gato[i] = new Gato("Gato " + (i + 1), i + 1, i % 2 == 0 ? "Macho" : "Hembra");
     }
 
     // Mostrar los datos de todos los gatos
     for (Gato g : gato) {
-      System.out.println("Nombre: " + g.getNombre() + ", Edad: " + g.getEdad() + ", Sexo: " + g.getSexo());
+      int sardinas = (int) (Math.random() * 11);
+      g.come(sardinas);
+      System.out.println("Nombre: " + g.getNombre() + ", Edad: " + g.getEdad() + ", Sexo: " + g.getSexo()
+          + ", Sardinas comidas: " + g.getSardinasComidas());
     }
   }
 }
-
-
-
-
--- Capítulo 9/Tarea_C9.2/Ejercicio1/Gato.java
+++ Capítulo 9/Tarea_C9.2/Ejercicio1/Gato.java
@@ -1,12 +1,31 @@
 public class Gato {
+  // declaracion variables y atributo de clase
   String nombre;
   int edad;
   String sexo;
+  static int sardinas = 10;
+  int sardinasComidas = 0;
 
+  // constructor
   public Gato(String nombre, int edad, String sexo) {
     this.nombre = nombre;
     this.edad = edad;
     this.sexo = sexo;
+  }
+
+  // método come (sardinas)
+  public void come(int s) {
+    System.out.print(this.nombre + ": ");
+    if (sardinas >= s) {
+      for (int i = 0; i < s; i++) {
+        System.out.print("Ñam...");
+        sardinas--;
+        sardinasComidas++;
+      }
+      System.out.println();
+    } else {
+      System.out.println("Ya no hay sardinas. ");
+    }
   }
 
   public String getNombre() {
@@ -19,5 +38,13 @@ public class Gato {
 
   public String getSexo() {
     return sexo;
+  }
+
+  public int getSardinasComidas() {
+    return sardinasComidas;
+  }
+
+  public static int getSardinas() {
+    return sardinas;
   }
 }
