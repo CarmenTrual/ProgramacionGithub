03eda6ceee409798d4b5f21a57949bbfd26e76f4
CarmenTrual <carmen.trujillo.alvarez@hotmail.com>
2023-04-13 20:42:44 +0200

    Tarea Java

--- Capítulo 14/Tarea 14.1/Ejercicio7.1/ExpocochesMalaga.java
+++ Capítulo 14/Tarea 14.1/Ejercicio7.1/ExpocochesMalaga.java
@@ -1,47 +1,75 @@
+
 /**
 * ExpocochesMalaga.java
 * Ejercicio 7 del capítulo 9.
 * @CarmenTrual
 */
 
+import java.util.Scanner;
+
 public class ExpocochesMalaga {
   public static void main(String[] args) {
-    
+    Scanner s = new Scanner(System.in);
+
     Zona principal = new Zona(1000);
     Zona compraVenta = new Zona(200);
     Zona vip = new Zona(25);
-    
-    int opcion = 0;
-    int opcion2 = 0;
+
+    String opcion = "";
+    String opcion2 = "";
     int n = 0;
-    
+    int opcionNum;
+    int opcionNum2;
+
     do {
       System.out.println("\nEXPOCOCHES MÁLAGA");
       System.out.println("1. Mostrar número de entradas libres");
       System.out.println("2. Vender entradas");
       System.out.println("3. Salir");
       System.out.print("Elige una opción: ");
-      
-      opcion = Integer.parseInt(System.console().readLine());
-      
-      if (opcion == 1) {
+
+      opcion = s.nextLine();
+
+      try {
+        opcionNum = Integer.parseInt(opcion);
+      } catch (Exception e) {
+        opcionNum = 0;
+        System.out.println("Introduce un número.");
+      }
+
+      if (opcionNum == 1) {
         System.out.println("\nEn la zona principal hay " + principal.getEntradasPorVender());
         System.out.println("En la zona de compra venta hay " + compraVenta.getEntradasPorVender());
         System.out.println("En la zona vip hay " + vip.getEntradasPorVender());
       }
-      
-      if (opcion == 2) {
+
+      if (opcionNum == 2) {
         System.out.println("\n1. Principal");
         System.out.println("2. Compra-venta");
         System.out.println("3. Vip");
         System.out.print("Elige la zona para la que quieres comprar las entradas: ");
-        
-        opcion2 = Integer.parseInt(System.console().readLine());
-        
+
+        opcion2 = s.nextLine();
+
+        try {
+          opcionNum2 = Integer.parseInt(opcion2);
+        } catch (Exception e) {
+          opcionNum2 = 0;
+          System.out.println("Introduce un número.");
+        }
+
         System.out.print("¿Cuántas entradas quieres? ");
-        n = Integer.parseInt(System.console().readLine());
-        
-        switch (opcion2) {
+        // n = s.nextInt();
+        // s.nextLine();
+
+        try {
+          n = Integer.parseInt(s.nextLine());
+        } catch (NumberFormatException e) {
+          n = 0;
+          System.out.println("Error: Introduce un número válido.");
+        }
+
+        switch (opcionNum2) {
           case 1:
             principal.vender(n);
             break;
@@ -52,8 +80,12 @@ public class ExpocochesMalaga {
             vip.vender(n);
             break;
           default:
+            System.out.println("Opción no válida. Introduce un número.");
+            break;
         }
       }
-    } while (opcion < 3); // menú principal
+    } while (opcionNum != 3); // menú principal
+
+    s.close();
   }
 }
---
+++ Capítulo 14/Tarea 14.1/Ejercicio7.2/ExpocochesMalaga.java
@@ -0,0 +1,97 @@
+
+/**
+* ExpocochesMalaga.java
+* Ejercicio 7 del capítulo 9.
+* @CarmenTrual
+*/
+
+import java.util.Scanner;
+
+public class ExpocochesMalaga {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    Zona principal = new Zona(1000);
+    Zona compraVenta = new Zona(200);
+    Zona vip = new Zona(25);
+
+    String opcion = "";
+    String opcion2 = "";
+    int n = 0;
+    int opcionNum;
+    int opcionNum2;
+
+    do {
+      System.out.println("\nEXPOCOCHES MÁLAGA");
+      System.out.println("1. Mostrar número de entradas libres");
+      System.out.println("2. Vender entradas");
+      System.out.println("3. Salir");
+      System.out.print("Elige una opción: ");
+
+      opcion = s.nextLine();
+
+      try {
+        opcionNum = Integer.parseInt(opcion);
+      } catch (Exception e) {
+        System.out.println("Excepcion: " + e.getClass());
+        System.out.println("Error: " + e.getMessage());
+        opcionNum = 0;
+        System.out.println("Introduce un número.");
+      }
+
+      if (opcionNum == 1) {
+        System.out.println("\nEn la zona principal hay " + principal.getEntradasPorVender());
+        System.out.println("En la zona de compra venta hay " + compraVenta.getEntradasPorVender());
+        System.out.println("En la zona vip hay " + vip.getEntradasPorVender());
+      }
+
+      if (opcionNum == 2) {
+        System.out.println("\n1. Principal");
+        System.out.println("2. Compra-venta");
+        System.out.println("3. Vip");
+        System.out.print("Elige la zona para la que quieres comprar las entradas: ");
+
+        opcion2 = s.nextLine();
+
+        try {
+          opcionNum2 = Integer.parseInt(opcion2);
+        } catch (Exception e) {
+          System.out.println("Excepcion: " + e.getClass());
+          System.out.println("Error: " + e.getMessage());
+          opcionNum2 = 0;
+          System.out.println("Introduce un número.");
+        }
+
+        System.out.print("¿Cuántas entradas quieres? ");
+        // n = s.nextInt();
+        // s.nextLine();
+
+        try {
+          n = Integer.parseInt(s.nextLine());
+        } catch (NumberFormatException e) {
+          System.out.println("Excepcion: " + e.getClass());
+          System.out.println("Error: " + e.getMessage());
+          n = 0;
+          System.out.println("Error: Introduce un número válido.");
+        }
+
+        switch (opcionNum2) {
+          case 1:
+            principal.vender(n);
+            break;
+          case 2:
+            compraVenta.vender(n);
+            break;
+          case 3:
+            vip.vender(n);
+            break;
+          default:
+            System.out.println("Opción no válida. Introduce un número.");
+            break;
+        }
+      }
+    } while (opcionNum != 3); // menú principal
+
+    s.close();
+  }
+}
