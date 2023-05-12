50dab0e87e3e9ace8e029541c4946fb76b81fdaf
CarmenTrual <carmen.trujillo.alvarez@hotmail.com>
2023-05-12 19:26:25 +0200

    Java

---
+++ Capítulo 10/Tarea C10.1/Parte 2/Ejercicio1/Planetas.java
@@ -0,0 +1,9 @@
+/**
+* @author Carmen Trujillo Álvarez
+* Define una jerarquía de clases que permita almacenar datos sobre los planetas y satélites (lunas)
+* que forman parte del sistema solar.
+* Define un método, que dado un objeto del sistema solar (planeta o satélite), imprima toda la 
+* información que se dispone sobre el mismo (además de su lista de satélites si los tuviera).
+*/
+
+
