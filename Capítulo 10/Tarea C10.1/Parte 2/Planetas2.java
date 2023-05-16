ddce66db0a7d37f9702cf24d0b4ee7b5ffdb8b0f
CarmenTrual <carmen.trujillo.alvarez@hotmail.com>
2023-05-16 09:26:30 +0200

    Java

--- Capítulo 10/Tarea C10.1/Parte 2/Planetas2.java
+++ Capítulo 10/Tarea C10.1/Parte 2/Planetas2.java
@@ -3,16 +3,15 @@ import java.util.ArrayList;
 public class Planetas2 extends Astros2 {
   // Atributos específicos de la clase Planetas2
   private double distanciaSol;
-  private ArrayList<Satelites2> satelites2; // Array de satélites del planeta
-  private boolean tieneSatelites2;
+  private ArrayList<Satelites2> satelites; // Array de satélites del planeta
+  private boolean tieneSatelites;
 
   // Constructor que inicializa los atributos específicos de la clase Planetas2 y
   // llama al constructor de Astros2 para inicializar sus atributos
   public Planetas2(double masa, double diametroMedio, double periodoRotacion, double periodoTraslacion,
-      double distanciaMedia, boolean tieneSatelites2, String string) {
+      double distanciaMedia, boolean tieneSatelites, String string) {
     super(masa, diametroMedio, periodoRotacion, periodoTraslacion, distanciaMedia, string);
-    this.tieneSatelites2 = tieneSatelites2;
-    this.satelites2 = new ArrayList<Satelites2>(); // Inicializar la lista de satélites
+    this.satelites = new ArrayList<Satelites2>();
   }
 
   // Métodos para obtener y establecer los valores de los atributos específicos de
@@ -25,18 +24,23 @@ public class Planetas2 extends Astros2 {
     this.distanciaSol = distanciaSol;
   }
 
-  public ArrayList<Satelites2> getSatelites2() {
-    return satelites2;
+  public ArrayList<Satelites2> getSatelites() {
+    return satelites;
   }
 
-  public void setSatelites2(ArrayList<Satelites2> satelites2) {
-    this.satelites2 = satelites2;
+  public void setSatelites(ArrayList<Satelites2> satelites) {
+    this.satelites = satelites;
   }
 
   // Método para añadir un satélite al array de satélites del planeta
-  public void anadirSatelite(Satelites2 satelite, int indice) {
-    satelites2.add(indice, satelite);
+  //public void anadirSatelite(Satelites2 satelite, int indice) {
+  //satelites.add(satelite);
+  //}
+  public void anadirSatelite(Satelites2 satelite) {
+  satelites.add(satelite);
+  tieneSatelites = true;
   }
+
 
   // Método para mostrar la información específica del planeta y la lista de
   // satélites que tiene
@@ -50,12 +54,18 @@ public class Planetas2 extends Astros2 {
     System.out.println("Distancia media: " + getDistanciaMedia() + " km");
     System.out.println("Distancia al Sol: " + distanciaSol + " km");
     System.out.println("Satélites: ");
-    for (Satelites2 satelite : satelites2) {
-      satelite.muestra();
-    }
+    if (satelites.isEmpty()) {
+      System.out.println("no tiene");
+}     else {
+        for (Satelites2 satelite : satelites) {
+          satelite.muestra();
+        }
+      }
+    // Imprimir el separador
+  System.out.println("------------------");
   }
 
-  public boolean getTieneSatelites2() {
-    return tieneSatelites2;
+  public boolean getTieneSatelites() {
+    return tieneSatelites;
   }
 }
--- Capítulo 10/Tarea C10.1/Parte 2/SistemaSolar2.java
+++ Capítulo 10/Tarea C10.1/Parte 2/SistemaSolar2.java
@@ -1,63 +1,56 @@
-
 /* SistemaSolar2.java
-* Modificar el programa Astros de la tarea 9.3 para hacerlo con arraylist.
+* Define una jerarquía de clases que permita almacenar datos sobre los planetas y satélites que 
+* forman parte del sistema solar.
 * @CarmenTrual
 */
-import java.util.Scanner;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SistemaSolar2 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
 
     // Crear los planetas
-    ArrayList<Planetas2> planetas2 = new ArrayList<>();
-    planetas2.add(new Planetas2(3.302E23, 4879, 1407.6, 87.97, 5.791E7, false, "Mercurio"));
-    planetas2.get(0).inicializarSatelites2(0);
-    planetas2.add(new Planetas2(4.869E24, 12104, -5832.5, 224.7, 1.082E8, false, "Venus"));
-    planetas2.get(1).inicializarSatelites2(0);
-    planetas2.add(new Planetas2(5.97E24, 12742, 24, 365.25, 1.496E8, true, "Tierra"));
-    planetas2.get(2).inicializarSatelites2(1);
-    planetas2.add(new Planetas2(6.39E23, 6779, 24.6, 687, 2.279E8, true, "Marte"));
-    planetas2.get(3).inicializarSatelites2(2);
-    planetas2.add(new Planetas2(1.898E27, 139822, 9.9, 4333, 7.785E8, true, "Júpiter"));
-    planetas2.get(4).inicializarSatelites2(4);
-    planetas2.add(new Planetas2(5.683E26, 116460, 10.7, 10759, 1.429E9, true, "Saturno"));
-    planetas2.get(5).inicializarSatelites2(3);
-    planetas2.add(new Planetas2(8.681E25, 50724, 17.24, 30687, 2.871E9, true, "Urano"));
-    planetas2.get(6).inicializarSatelites2(2);
-    planetas2.add(new Planetas2(1.024E26, 49244, 16.11, 60190, 4.498E9, true, "Neptuno"));
-    planetas2.get(7).inicializarSatelites2(1);
+    ArrayList<Planetas2> planetas = new ArrayList<>();
+    planetas.add(new Planetas2(3.302E23, 4879, 1407.6, 87.97, 5.791E7, false, "Mercurio"));
+    planetas.add(new Planetas2(4.869E24, 12104, -5832.5, 224.7, 1.082E8, false, "Venus"));
+    planetas.add(new Planetas2(5.97E24, 12742, 24, 365.25, 1.496E8, true, "Tierra"));
+    planetas.add(new Planetas2(6.39E23, 6779, 24.6, 687, 2.279E8, true, "Marte"));
+    planetas.add(new Planetas2(1.898E27, 139822, 9.9, 4333, 7.785E8, true, "Júpiter"));
+    planetas.add(new Planetas2(5.683E26, 116460, 10.7, 10759, 1.429E9, true, "Saturno"));
+    planetas.add(new Planetas2(8.681E25, 50724, 17.24, 30687, 2.871E9, true, "Urano"));
+    planetas.add(new Planetas2(1.024E26, 49244, 16.11, 60190, 4.498E9, true, "Neptuno"));
 
     // Agregar satélites al planeta Tierra
-    planetas2.get(2).anadirSatelite(new Satelites2("Luna", 7.342E22, 3474), 0);
+    planetas.get(2).anadirSatelite(new Satelites2("Luna", 7.342E22, 3474));
 
     // Agregar satélites al planeta Marte
-    planetas2.get(3).anadirSatelite(new Satelites2("Fobos", 1.0659E16, 22), 0);
-    planetas2.get(3).anadirSatelite(new Satelites2("Deimos", 1.4762E15, 12), 1);
+    planetas.get(3).anadirSatelite(new Satelites2("Fobos", 1.0659E16, 22));
+    planetas.get(3).anadirSatelite(new Satelites2("Deimos", 1.4762E15, 12));
 
     // Agregar satélites al planeta Júpiter
-    planetas2.get(4).anadirSatelite(new Satelites2("Io", 8.9319E22, 3643), 0);
-    planetas2.get(4).anadirSatelite(new Satelites2("Europa", 4.7998E22, 3122), 1);
-    planetas2.get(4).anadirSatelite(new Satelites2("Ganimedes", 1.4819E23, 5268), 2);
-    planetas2.get(4).anadirSatelite(new Satelites2("Calisto", 1.0759E23, 4821), 3);
+    planetas.get(4).anadirSatelite(new Satelites2("Io", 8.9319E22, 3643));
+    planetas.get(4).anadirSatelite(new Satelites2("Europa", 4.7998E22, 3122));
+    planetas.get(4).anadirSatelite(new Satelites2("Ganimedes", 1.4819E23, 5268));
+    planetas.get(4).anadirSatelite(new Satelites2("Calisto", 1.0759E23, 4821));
 
     // Agregar satélites al planeta Saturno
-    planetas2.get(5).anadirSatelite(new Satelites2("Mimas", 3.7493E19, 396), 0);
-    planetas2.get(5).anadirSatelite(new Satelites2("Encélado", 1.08022E20, 504), 1);
-    planetas2.get(5).anadirSatelite(new Satelites2("Tetis", 6.17449E20, 1066), 2);
+    planetas.get(5).anadirSatelite(new Satelites2("Mimas", 3.7493E19, 396));
+    planetas.get(5).anadirSatelite(new Satelites2("Encélado", 1.08022E20, 504));
+    planetas.get(5).anadirSatelite(new Satelites2("Tetis", 6.17449E20, 1066));
 
     // Agregar satélites al planeta Urano
-    planetas2.get(6).anadirSatelite(new Satelites2("Titania", 3.527E21, 1577), 0);
-    planetas2.get(6).anadirSatelite(new Satelites2("Oberón", 3.014E21, 1523), 1);
+    planetas.get(6).anadirSatelite(new Satelites2("Titania", 3.527E21, 1577));
+    planetas.get(6).anadirSatelite(new Satelites2("Oberón", 3.014E21, 1523));
 
     // Agregar satélites al planeta Neptuno
-    planetas2.get(7).anadirSatelite(new Satelites2("Tritón", 2.14E22, 2707), 0);
+    planetas.get(7).anadirSatelite(new Satelites2("Tritón", 2.14E22, 2707));
 
     int opcion = 0;
     while (opcion != 4) {
       System.out.println("Seleccione una opción:");
-      System.out.println("1. Mostrar información de los planetas2.");
+      System.out.println("1. Mostrar información de los planetas.");
       System.out.println("2. Mostrar información de los satélites.");
       System.out.println("3. Mostrar información de un astro específico.");
       System.out.println("4. Salir.");
@@ -65,16 +58,16 @@ public class SistemaSolar2 {
 
       switch (opcion) {
         case 1:
-          System.out.println("Información de los planetas2:");
-          for (Planetas2 planeta : planetas2) {
+          System.out.println("Información de los planetas:");
+          for (Planetas2 planeta : planetas) {
             planeta.muestra();
           }
           break;
         case 2:
           System.out.println("Información de los satélites:");
-          for (Planetas2 planeta : planetas2) {
-            if (planeta.getTieneSatelites2()) {
-              for (Satelites2 satelite : planeta.getSatelites2()) {
+          for (Planetas2 planeta : planetas) {
+            if (planeta.getTieneSatelites()) {
+              for (Satelites2 satelite : planeta.getSatelites()) {
                 satelite.muestra();
               }
             }
@@ -84,13 +77,13 @@ public class SistemaSolar2 {
           System.out.println("Ingrese el nombre del astro:");
           String nombre = s.next();
           boolean encontrado = false;
-          for (Planetas2 planeta : planetas2) {
+          for (Planetas2 planeta : planetas) {
             if (planeta.getNombre().equals(nombre)) {
               encontrado = true;
               planeta.muestra();
               break;
-            } else if (planeta.getTieneSatelites2()) {
-              for (Satelites2 satelite : planeta.getSatelites2()) {
+            } else if (planeta.getTieneSatelites()) {
+              for (Satelites2 satelite : planeta.getSatelites()) {
                 if (satelite.getNombre().equals(nombre)) {
                   encontrado = true;
                   satelite.muestra();
