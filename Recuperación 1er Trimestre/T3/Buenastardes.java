d564ff0b59a3ba11e49525e3a83d92a353d1026d
CarmenTrual <carmen.trujillo.alvarez@hotmail.com>
2023-06-05 15:21:55 +0200

    Java

---
+++ Recuperación 1er Trimestre/T3/Buenastardes.java
@@ -0,0 +1,10 @@
+/* HolaMundo.java
+* programa que imprime por pantalla Buenas tardes
+* @CarmenTrual
+*/
+	public class Buenastardes {
+		public static void main (String[] args) {
+            // TODO code application logic here
+            System.out.println ("Buenas tardes!");
+        }
+    }
---
+++ Recuperación 1er Trimestre/T3/LongCirc.java
@@ -0,0 +1,15 @@
+/* LongCirc.java
+* programa que imprime por pantalla la longitud de una circunferencia
+* @CarmenTrual
+*/
+	public class LongCirc {
+		public static void main (String[] args) {
+            int radio;
+            radio=5;
+            double pi;
+            pi=3,14;
+            double longitud;
+            longitud=(2pi)*5;            
+            System.out.println ("La longitud de la circunferencia es " + longitud);
+        }
+    }
