bb33588cef998210bea8e4f2e3d8d2a54ba9295c
CarmenTrual <carmen.trujillo.alvarez@hotmail.com>
2023-06-13 12:54:44 +0200

    Java

---
+++ Recuperación 3er Trimestre/C9/Ejercicio1/Producto.java
@@ -0,0 +1,12 @@
+public class Producto {
+
+  // atributos
+  private String nombre;
+  private int cantidad;
+
+  // constructor
+  public Producto(String nombre, int cantidad) {
+    this.nombre = nombre;
+    this.cantidad = cantidad;
+  }
+}
